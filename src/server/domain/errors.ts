73b2d611bd0bb025324def542058ea1e6c3f8604
Felix Serang <byte007@zoho.com>
2026-09-05 13:58:38 +0700

    feat: add atomic sales and cancellations

--- src/server/domain/errors.ts
+++ src/server/domain/errors.ts
@@ -16,3 +16,15 @@ export class ForbiddenError extends DomainError {
     super('FORBIDDEN', 'Insufficient permissions')
   }
 }
+
+export class InsufficientStockError extends DomainError {
+  constructor() {
+    super('INSUFFICIENT_STOCK', 'Insufficient stock')
+  }
+}
+
+export class SaleAlreadyCancelledError extends DomainError {
+  constructor() {
+    super('SALE_ALREADY_CANCELLED', 'Sale has already been cancelled')
+  }
+}
